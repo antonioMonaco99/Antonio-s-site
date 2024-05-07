e54f4fe8cac3782b39f2493e848494649e11cc70
antonioMonaco99 <151932314+antonioMonaco99@users.noreply.github.com>
2024-05-07 10:36:22 +0200

    Add files via upload

---
+++ js/write_effect_intro.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    new TypeIt('#write_effect', {
+        strings: ["Computer Science student"],
+        speed: 50,
+        waitUntilVisible: true
+    }).go();
+});
